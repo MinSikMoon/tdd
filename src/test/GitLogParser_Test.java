4c59576570b5cc69a2f4d4d51ad93c5b11506853
minsikmoon <msmoon.business@gmail.com>
2019-05-26 23:08:36 +0900

    test case 준비

--- src/test/GitLogParser_Test.java
+++ src/test/GitLogParser_Test.java
@@ -1,14 +1,36 @@
 package test;
 
-import static org.junit.Assert.*;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.Reader;
+import java.io.Writer;
 
-import org.junit.Test;
+import org.junit.After;
+import org.junit.Before;
 
 public class GitLogParser_Test {
-
-	@Test
-	public void test() {
-		fail("Not yet implemented");
+	//테스트용 준비물들
+	Reader reader;
+	Writer writer;
+	String compareText = "";
+	char[] buffer = new char[1000];
+	
+	@Before
+	public void 준비() throws Exception {
+		reader = new FileReader("param.txt");
+		writer = new FileWriter("test.txt");
+		while(reader.read(buffer) != -1) 
+			compareText += new String(buffer);
+		writer.write(compareText);
 	}
-
+	
+	//로직 테스트할 곳
+	
+	
+	@After
+	public void 리더닫기() throws IOException {
+		reader.close();
+		writer.close();
+	}
 }
