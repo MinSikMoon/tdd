9fb5dea5fca1180cde0a53c7737933fa98cdbe84
minsikmoon <msmoon.business@gmail.com>
2019-05-30 23:32:40 +0900

    test3

--- src/test/module/GradeManagerTest.java
+++ src/test/module/GradeManagerTest.java
@@ -18,7 +18,7 @@ class GradeManagerTest {
 		assertEquals(GradeManager.MESSAGE_1, GradeManager.getGradeMessage("사원",499, null));
 		assertEquals(GradeManager.MESSAGE_1, GradeManager.getGradeMessage("사원",499, "some prize"));
 		
-		//사원
+		//관리자
 		assertEquals(GradeManager.MESSAGE_1, GradeManager.getGradeMessage("관리자",2000, ""));
 		assertEquals(GradeManager.MESSAGE_1, GradeManager.getGradeMessage("관리자",2000, null));
 		assertEquals(GradeManager.MESSAGE_1, GradeManager.getGradeMessage("관리자",2000, "some prize"));
@@ -26,5 +26,34 @@ class GradeManagerTest {
 		assertEquals(GradeManager.MESSAGE_1, GradeManager.getGradeMessage("관리자",2499, null));
 		assertEquals(GradeManager.MESSAGE_1, GradeManager.getGradeMessage("관리자",2499, "some prize"));
 	}
+	
+	@Test
+	void 메시지2_테스트() {
+		//사원
+		assertEquals(GradeManager.MESSAGE_2, GradeManager.getGradeMessage("사원",500, ""));
+		assertEquals(GradeManager.MESSAGE_2, GradeManager.getGradeMessage("사원",500, "some prize"));
+		assertEquals(GradeManager.MESSAGE_2, GradeManager.getGradeMessage("사원",999, ""));
+		assertEquals(GradeManager.MESSAGE_2, GradeManager.getGradeMessage("사원",999, "some prize"));
+		
+		//관리자
+		assertEquals(GradeManager.MESSAGE_2, GradeManager.getGradeMessage("관리자",2500, ""));
+		assertEquals(GradeManager.MESSAGE_2, GradeManager.getGradeMessage("관리자",2500, "some prize"));
+		assertEquals(GradeManager.MESSAGE_2, GradeManager.getGradeMessage("관리자",2999, ""));
+		assertEquals(GradeManager.MESSAGE_2, GradeManager.getGradeMessage("관리자",2999, "some prize"));
+	}
 
+	@Test
+	void 메시지3_테스트() {
+		//사원
+		assertEquals(GradeManager.MESSAGE_3, GradeManager.getGradeMessage("사원",1000, ""));
+		assertEquals(GradeManager.MESSAGE_3, GradeManager.getGradeMessage("사원",1000, "some prize"));
+		assertEquals(GradeManager.MESSAGE_3, GradeManager.getGradeMessage("사원",1499, ""));
+		assertEquals(GradeManager.MESSAGE_3, GradeManager.getGradeMessage("사원",1499, "some prize"));
+		
+		//관리자
+		assertEquals(GradeManager.MESSAGE_3, GradeManager.getGradeMessage("관리자",3000, ""));
+		assertEquals(GradeManager.MESSAGE_3, GradeManager.getGradeMessage("관리자",3000, "some prize"));
+		assertEquals(GradeManager.MESSAGE_3, GradeManager.getGradeMessage("관리자",3499, ""));
+		assertEquals(GradeManager.MESSAGE_3, GradeManager.getGradeMessage("관리자",3499, "some prize"));
+	}
 }
