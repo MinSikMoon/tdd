e8f9df36883e90e18f26384d6bb4bcfc6fe0a4b0
minsikmoon <msmoon.business@gmail.com>
2019-06-01 18:11:23 +0900

    리팩토링 끝

--- src/main/module/GradeManager.java
+++ src/main/module/GradeManager.java
@@ -5,21 +5,26 @@ public class GradeManager {
 	public static final String MESSAGE_1 = "메시지1";
 	public static final String MESSAGE_2 = "메시지2";
 	public static final String MESSAGE_3 = "메시지3";
-	private static double[] employeePoints = {500, 1000, 1500};
+	private static double[] employeePoints = { 0, 500, 1000, 1500 };
+	private static double[] managerPoints = { 0, 2500, 3000, 3500 };
+	private static String[] messages = { MESSAGE_1, MESSAGE_2, MESSAGE_3 };
 
-	public static Object getGradeMessage(String position, double point, String prize ) {
-		if(position.equals("사원")) {
-			if(employeePoints [0] <= point && point < employeePoints[1])
-				return MESSAGE_2;
-			if(employeePoints[1] <= point && point < employeePoints[2])
-				return MESSAGE_3;
-		}else if(position.equals("관리자")) {
-			if(2500 <= point && point < 3000)
-				return MESSAGE_2;
-			if(3000 <= point && point < 3500)
-				return MESSAGE_3;
+	public static Object getGradeMessage(String position, double point, String prize) {
+		String resultMessage = messages[0];
+		double[] points = getPointsArrayByPosition(position);
+		for (int i = 0; i < points.length - 1; i++) {
+			if (isBetween(point, points[i], points[i + 1]))
+				resultMessage = messages[i];
 		}
-		return MESSAGE_1;
+		return resultMessage;
 	}
-	
+
+	private static double[] getPointsArrayByPosition(String position) {
+		return position.equals("사원") ? employeePoints : managerPoints;
+	}
+
+	private static boolean isBetween(double point, double sameAndUpperThan, double belowThan) {
+		return (sameAndUpperThan <= point && point < belowThan) ? true : false;
+	}
+
 }
