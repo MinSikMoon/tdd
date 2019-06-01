c1cbe6ed408477ba71167f4b4f07f52ee60d61fc
minsikmoon <msmoon.business@gmail.com>
2019-06-01 16:39:20 +0900

    리팩토링

--- src/main/module/GradeManager.java
+++ src/main/module/GradeManager.java
@@ -5,12 +5,13 @@ public class GradeManager {
 	public static final String MESSAGE_1 = "메시지1";
 	public static final String MESSAGE_2 = "메시지2";
 	public static final String MESSAGE_3 = "메시지3";
+	private static double[] employeePoints = {500, 1000, 1500};
 
 	public static Object getGradeMessage(String position, double point, String prize ) {
 		if(position.equals("사원")) {
-			if(500 <= point && point < 1000)
+			if(employeePoints [0] <= point && point < employeePoints[1])
 				return MESSAGE_2;
-			if(1000 <= point && point < 1500)
+			if(employeePoints[1] <= point && point < employeePoints[2])
 				return MESSAGE_3;
 		}else if(position.equals("관리자")) {
 			if(2500 <= point && point < 3000)
@@ -20,5 +21,5 @@ public class GradeManager {
 		}
 		return MESSAGE_1;
 	}
-
+	
 }
